10d7a2c9fc2ed54a4624f77192f5224a6e36d89e
Ravindra Singh <it.ravindrasingh@gmail.com>
2018-04-14 14:56:32 +0530

    Added first codebase to heroku.

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const app = express()
+
+app.set("port", (process.env.PORT || 5000))
+
+// Allow us to process the data
+
+app.use(bodyParser.urlencoded({exntended: false}))
+
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', function(req,res){
+  res.send("Hi, I am bot")
+})
+
+// Facebook
+
+app.get('/webhook/', function(req,res){
+  if(req.query['hub.verify_token'] == "ravindrasinghpage"){
+    res.send(req.query['hub.challenge'])
+  }
+})
+
+// Validating port is running.
+app.listen(app.get('port'), function(){
+  console.log("Running: port")
+})
